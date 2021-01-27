07b705d6e4da9e2e0dd462656befbeabae0c9f51
Mikhail Melnikov <mike@localhost.localdomain>
2021-01-27 04:13:52 +0300

    Update models

--- worker/model.py
+++ worker/model.py
@@ -5,18 +5,13 @@ import torch.nn as nn
 
 from torch import Tensor
 
+import functools
+import torch.nn.functional as F
+
 
 class ResidualDenseBlock(nn.Module):
-    r"""The residual block structure of traditional SRGAN and Dense model is defined"""
 
     def __init__(self, in_channels, growth_channels, scale_ratio):
-        """
-
-        Args:
-            in_channels (int): Number of channels in the input image. (Default: 64).
-            growth_channels (int): how many filters to add each layer (`k` in paper). (Default: 32).
-            scale_ratio (float): Residual channel scaling column. (Default: 0.2)
-        """
         super(ResidualDenseBlock, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels + 0 * growth_channels, growth_channels, 3, 1, 1, bias=False),
@@ -60,16 +55,7 @@ class ResidualDenseBlock(nn.Module):
 
 
 class ResidualInResidualDenseBlock(nn.Module):
-    r"""The residual block structure of traditional ESRGAN and Dense model is defined"""
-
     def __init__(self, in_channels, growth_channels, scale_ratio):
-        """
-
-        Args:
-            in_channels (int): Number of channels in the input image.
-            growth_channels (int): how many filters to add each layer (`k` in paper).
-            scale_ratio (float): Residual channel scaling column.
-        """
         super(ResidualInResidualDenseBlock, self).__init__()
         self.RDB1 = ResidualDenseBlock(in_channels, growth_channels, scale_ratio)
         self.RDB2 = ResidualDenseBlock(in_channels, growth_channels, scale_ratio)
@@ -86,22 +72,7 @@ class ResidualInResidualDenseBlock(nn.Module):
 
 
 class ESRGAN(nn.Module):
-    def __init__(self, upscale_factor=4, num_rrdb_blocks=4):
-        r""" This is an esrgan model defined by the author himself.
-
-        We use two settings for our generator – one of them contains 8 residual blocks, with a capacity similar
-        to that of SRGAN and the other is a deeper model with 8/16/23 RRDB blocks.
-
-        Args:
-            upscale_factor (int): Image magnification factor. (Default: 4).
-            num_rrdb_blocks (int): How many residual in residual blocks are combined. (Default: 4).
-
-        Notes:
-            Use `num_rrdb_blocks` is 4 for RTX 2080.
-            Use `num_rrdb_blocks` is 8 for RTX 2080Ti.
-            Use `num_rrdb_blocks` is 16 for TITAN RTX.
-            Use `num_rrdb_blocks` is 23 for Tesla A100.
-        """
+    def __init__(self, upscale_factor=4, num_rrdb_blocks=23):
         super(ESRGAN, self).__init__()
         num_upsample_block = int(math.log(upscale_factor, 2))
 
@@ -121,22 +92,16 @@ class ESRGAN(nn.Module):
         upsampling = []
         for _ in range(num_upsample_block):
             upsampling += [
-                nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.Upsample(scale_factor=2, mode='nearest'),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
                 nn.LeakyReLU(negative_slope=0.2, inplace=True),
-                nn.PixelShuffle(upscale_factor=2)
             ]
         self.upsampling = nn.Sequential(*upsampling)
 
-        # Next layer after upper sampling
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        )
-
         # Final output layer
-        self.conv4 = nn.Sequential(
+        self.convOut = nn.Sequential(
             nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.Tanh()
+            #             nn.Tanh()
         )
 
     def forward(self, input: Tensor) -> Tensor:
@@ -145,7 +110,82 @@ class ESRGAN(nn.Module):
         out2 = self.conv2(out)
         out = torch.add(out1, out2)
         out = self.upsampling(out)
-        out = self.conv3(out)
-        out = self.conv4(out)
+        out = self.convOut(out)
+
+        return out
+
+
+########################################################
+
+def make_layer(block, n_layers):
+    layers = []
+    for _ in range(n_layers):
+        layers.append(block())
+    return nn.Sequential(*layers)
+
+
+class ResidualDenseBlock_5C(nn.Module):
+    def __init__(self, nf=64, gc=32, bias=True):
+        super(ResidualDenseBlock_5C, self).__init__()
+        # gc: growth channel, i.e. intermediate channels
+        self.conv1 = nn.Conv2d(nf, gc, 3, 1, 1, bias=bias)
+        self.conv2 = nn.Conv2d(nf + gc, gc, 3, 1, 1, bias=bias)
+        self.conv3 = nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1, bias=bias)
+        self.conv4 = nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1, bias=bias)
+        self.conv5 = nn.Conv2d(nf + 4 * gc, nf, 3, 1, 1, bias=bias)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+        # initialization
+        # mutil.initialize_weights([self.conv1, self.conv2, self.conv3, self.conv4, self.conv5], 0.1)
+
+    def forward(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
+        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        return x5 * 0.2 + x
+
+
+class RRDB(nn.Module):
+    '''Residual in Residual Dense Block'''
+
+    def __init__(self, nf, gc=32):
+        super(RRDB, self).__init__()
+        self.RDB1 = ResidualDenseBlock_5C(nf, gc)
+        self.RDB2 = ResidualDenseBlock_5C(nf, gc)
+        self.RDB3 = ResidualDenseBlock_5C(nf, gc)
+
+    def forward(self, x):
+        out = self.RDB1(x)
+        out = self.RDB2(out)
+        out = self.RDB3(out)
+        return out * 0.2 + x
+
+
+class RRDBNet(nn.Module):
+    def __init__(self, in_nc, out_nc, nf, nb, gc=32):
+        super(RRDBNet, self).__init__()
+        RRDB_block_f = functools.partial(RRDB, nf=nf, gc=gc)
+
+        self.conv_first = nn.Conv2d(in_nc, nf, 3, 1, 1, bias=True)
+        self.RRDB_trunk = make_layer(RRDB_block_f, nb)
+        self.trunk_conv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        #### upsampling
+        self.upconv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.upconv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.HRconv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.conv_last = nn.Conv2d(nf, out_nc, 3, 1, 1, bias=True)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x):
+        fea = self.conv_first(x)
+        trunk = self.trunk_conv(self.RRDB_trunk(fea))
+        fea = fea + trunk
+
+        fea = self.lrelu(self.upconv1(F.interpolate(fea, scale_factor=2, mode='nearest')))
+        fea = self.lrelu(self.upconv2(F.interpolate(fea, scale_factor=2, mode='nearest')))
+        out = self.conv_last(self.lrelu(self.HRconv(fea)))
 
         return out
