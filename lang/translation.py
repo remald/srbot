fb249d14fa4bfc338fd80c8e581e6b718ff28b32
Mikhail Melnikov <mike@localhost.localdomain>
2021-01-31 23:28:20 +0300

    Add translation module

---
+++ lang/translation.py
@@ -0,0 +1,18 @@
+import lang.en as en
+import lang.ru as ru
+import liveOptions
+
+
+def translate(client_id):
+    lang = liveOptions.__LIVE_OPTIONS__.get_lang(client_id)
+    if 'ru' in lang:
+        tr = ru.translation
+    else:
+        tr = en.translation
+
+    return tr
+
+
+def set_lang(message):
+    print(message.from_user.locale.language)
+    liveOptions.__LIVE_OPTIONS__.set_lang(message.from_user.id, message.from_user.locale.language)
