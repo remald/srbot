f006cca681f9b2b600a31d5008e9d4ad64eba49d
Mikhail Melnikov <mike@localhost.localdomain>
2021-02-12 21:14:24 +0300

    Fix got locale method

--- util/srbot_locale.py
+++ util/srbot_locale.py
@@ -1,12 +1,18 @@
 from aiogram.contrib.middlewares.i18n import I18nMiddleware
 from typing import Any, Tuple
+from aiogram import types
+from babel import Locale
+
+SUPPORTED_LANG = ['en', 'ru']
 
 
 class SrbotLocale(I18nMiddleware):
 
     async def get_user_locale(self, action: str, args: Tuple[Any]) -> str:
-        data: dict = args[-1]
-        print(self.default)
-        if "chat" in data:
-            return data["chat"].language or self.default
-        return self.default
+        user: types.User = types.User.get_current()
+        locale: Locale = user.locale
+
+        if locale:
+            *_, data = args
+            language = data['locale'] = locale.language
+            return language if language in SUPPORTED_LANG else self.default
