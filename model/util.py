5bd0f5094d98d94368b83f4b9957d5e7b3a47de3
Mikhail Melnikov <mike@localhost.localdomain>
2021-02-15 01:01:28 +0300

    Restore filename for original model

--- model/util.py
+++ model/util.py
@@ -7,7 +7,7 @@ import torch
 def load_models(device='cpu'):
     net1 = RRDBNet(3, 3, 64, 23, gc=32).to(device)
     net1.load_state_dict(
-        torch.load("./weights/esr1.pth", map_location=device))
+        torch.load("./weights/RRDB_ESRGAN_x4.pth", map_location=device))
 
     net2 = ESRGAN().to(device)
     dump = torch.load("./weights/netG_4x_checkpoint.pth", map_location=device)
